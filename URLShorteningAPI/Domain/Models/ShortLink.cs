876871109584e326d8ad60d8e2c0f7683c948b06
ComradeRyn <ryanyuncza@gmail.com>
2026-03-24 09:40:22 -0400

    Added models in the domain

---
+++ URLShorteningAPI/Domain/Models/ShortLink.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models;
+
+public class ShortLink
+{
+    public long Id;
+    public required string LongUrl;
+    public string? ShortCode;
+    public string? CustomAlias;
+    public string? Password;
+    public DateTime CreationDate;
+    public required ICollection<Visit> Visits;
+}
---
+++ URLShorteningAPI/Domain/Models/Visit.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Models;
+
+public class Visit
+{
+    public long Id;
+    public DateTime Date;
+    public required ShortLink ShortLink;
+}
