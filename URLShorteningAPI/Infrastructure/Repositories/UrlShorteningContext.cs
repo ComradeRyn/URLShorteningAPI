fe2e1b6830b8cf51deab5dd1f3a67a43b5ee3b97
ComradeRyn <ryanyuncza@gmail.com>
2026-03-24 09:49:23 -0400

    Added UrlShorteningContext to Repositories layer

---
+++ URLShorteningAPI/Infrastructure/Repositories/UrlShorteningContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class UrlShorteningContext (DbContextOptions<UrlShorteningContext> options) : DbContext(options)
+{
+    public DbSet<ShortLink> ShortLinks { get; init; }
+    public DbSet<Visit> Visits { get; init; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<ShortLink>()
+            .Property(x => x.Id)
+            .UseIdentityColumn(seed: 121, increment: 3);
+    }
+}
