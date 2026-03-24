b52daa9c55200e59e43f05539da263a582f2725e
ComradeRyn <ryanyuncza@gmail.com>
2026-03-24 14:19:59 -0400

    Added ShortLinksController.cs, and added ShortenUrl endpoint

---
+++ URLShorteningAPI/API/Controllers/ShortLinksController.cs
@@ -0,0 +1,35 @@
+﻿using Application.DTOs.Requests;
+using Application.DTOs.Responses;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/shortlinks")]
+[ApiController]
+// [Authorize]
+public class ShortLinksController : ControllerBase
+{
+    private readonly IShortLinksService _shortLinksService;
+
+    public ShortLinksController(IShortLinksService shortLinksService)
+    {
+        _shortLinksService = shortLinksService;
+    }
+    
+    // TODO: add comment documenting endpoint
+    [HttpPost]
+    [ProducesResponseType(typeof(ShortUrlResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<ShortUrlResponse>> ShortenUrl(CreationRequest request)
+    {
+        var response = await _shortLinksService.ShortenUrl(request);
+
+        if (!response.IsSuccess)
+        {
+            return BadRequest(response.ErrorMessage);
+        }
+
+        return Ok(response.Content);
+    }
+}
