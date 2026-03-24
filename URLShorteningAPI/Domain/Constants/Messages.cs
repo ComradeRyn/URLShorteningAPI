c166dfc05a05168afdc017db19e5e4d8c8efc1b7
ComradeRyn <ryanyuncza@gmail.com>
2026-03-24 09:44:53 -0400

    Added Messages class in constants

---
+++ URLShorteningAPI/Domain/Constants/Messages.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Constants;
+
+public static class Messages
+{
+    public const string CustomAliasTaken = "Requested custom alias is already taken.";
+    public const string InvalidCustomAliasFormat = "Requested custom alias cannot contain whitespace";
+    public const string IncorrectPassword = "Submitted password is incorrect";
+    public const string LinkNotFound = "Requested link does not exist";
+}
