6b21067f7be3f19fee294976c1e3ec55485c611c
ComradeRyn <ryanyuncza@gmail.com>
2026-03-26 11:25:36 -0400

    Added logic to GetShortLink method in AnalyticsService class

--- URLShorteningAPI/Application/Services/AnalyticsService.cs
+++ URLShorteningAPI/Application/Services/AnalyticsService.cs
@@ -1,6 +1,8 @@
-﻿using Application.DTOs.Requests;
+﻿using System.Net;
+using Application.DTOs.Requests;
 using Application.DTOs.Responses;
 using Application.Interfaces;
+using Domain.Constants;
 
 namespace Application.Services;
 
@@ -15,9 +17,35 @@ public class AnalyticsService : IAnalyticsService
         _analyticsService = analyticsService;
     }
     
-    public Task<ApiResponse<ShortLinkAnalyticsResponse>> GetShortLink(ShortLinkAnalyticsRequest request)
+    public async Task<ApiResponse<ShortLinkAnalyticsResponse>> GetShortLink(ShortLinkAnalyticsRequest request)
     {
-        throw new NotImplementedException();
+        var shortLink = await _shortLinksRepository.GetByShortCode(request.ShortAlias) ?? 
+                        await _shortLinksRepository.GetByCustomAlias(request.ShortAlias);
+
+        if (shortLink is null)
+        {
+            return new ApiResponse<ShortLinkAnalyticsResponse>(
+                HttpStatusCode.NotFound, Messages.LinkNotFound);
+        }
+        
+        // Input format should be "yyyyMMddHH"
+        var startDate = DateTime.Parse(request.StartDate);
+        var endDate = DateTime.Parse(request.EndDate);
+
+        var query = shortLink
+            .Visits
+            .AsQueryable()
+            .Where(visit => visit.Date >= startDate && visit.Date <= endDate);
+
+        // Question: what happens if the link was never visited?
+        var totalVisits = query.Count();
+        var lastVisitedAt = query.MaxBy(visit => visit.Date)?.Date;
+
+        return new ApiResponse<ShortLinkAnalyticsResponse>(
+            new ShortLinkAnalyticsResponse(
+                totalVisits,
+                shortLink.CreationDate,
+                lastVisitedAt));
     }
 
     public Task<ApiResponse<VisitAnalyticsResponse>> GetVisits(VisitAnalyticsRequest request)
