631ebcec6cf05de0bba9e4f7ee4cf3149cc47a78
ComradeRyn <ryanyuncza@gmail.com>
2026-03-26 10:48:30 -0400

    Added logic for GetAnalytics in VisitsRepository

--- URLShorteningAPI/Infrastructure/Repositories/VisitsRepository.cs
+++ URLShorteningAPI/Infrastructure/Repositories/VisitsRepository.cs
@@ -1,12 +1,32 @@
 ﻿using Application.DTOs.Responses;
 using Application.Interfaces;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories;
 
 public class VisitsRepository : IVisitsRepository
 {
-    public Task<VisitsAnalyticsModel> GetAnalytics(DateTime startDate, DateTime endDate)
+    private readonly UrlShorteningContext _context;
+
+    public VisitsRepository(UrlShorteningContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+    
+    public async Task<VisitsAnalyticsModel> GetAnalytics(DateTime startDate, DateTime endDate)
+    {
+        var query = _context.Visits as IQueryable<Visit>;
+        query = query.Where(visit => visit.Date >= startDate && visit.Date <= endDate);
+        
+        var count = await query.CountAsync();
+
+        var topFiveUrls = query
+            .GroupBy(visit => visit.ShortLink)
+            .OrderBy(group => group.Count())
+            .Select(group => group.Key.LongUrl)
+            .AsEnumerable();
+
+        return new VisitsAnalyticsModel(topFiveUrls, count);
     }
 }
