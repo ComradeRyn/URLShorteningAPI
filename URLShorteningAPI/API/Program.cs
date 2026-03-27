008e79bc5b43af43c467dd7a6d610dabfb3d9c1e
ComradeRyn <ryanyuncza@gmail.com>
2026-03-27 09:50:56 -0400

    Added authenticatoin and comments to swagger page

--- URLShorteningAPI/API/Program.cs
+++ URLShorteningAPI/API/Program.cs
@@ -1,9 +1,11 @@
+using System.Reflection;
 using Application.Interfaces;
 using Application.Services;
 using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
 using Sqids;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,7 +16,26 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+    
+    setupAction.IncludeXmlComments(xmlCommentsFullPath);
+    
+    setupAction.AddSecurityDefinition("BankingApiBearerAuth", new OpenApiSecurityScheme()
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "Input a valid token to access this API"
+    });
+
+    setupAction.AddSecurityRequirement(document => new OpenApiSecurityRequirement()
+    {
+        [new OpenApiSecuritySchemeReference("BankingApiBearerAuth", document)] = []
+    });
+});
 
 builder.Services.AddSingleton(new SqidsEncoder<long>(new SqidsOptions
 {
