44f02d7faa24c7e9ea452e12d0d7f1d6ee54782b
ComradeRyn <ryanyuncza@gmail.com>
2026-03-31 10:56:29 -0400

    Refactored InvalidCustomAlias message

--- URLShorteningAPI/Domain/Constants/Messages.cs
+++ URLShorteningAPI/Domain/Constants/Messages.cs
@@ -3,8 +3,8 @@
 public static class Messages
 {
     public const string CustomAliasTaken = "Requested custom alias is already taken.";
-    public const string InvalidCustomAlias = "Requested custom alias cannot contain whitespace or slashes";
-    public const string IncorrectPassword = "Submitted password is incorrect";
-    public const string LinkNotFound = "Requested link does not exist";
-    public const string InvalidDateFormat = "Date must be in form of yyyy-MM-dd";
+    public const string InvalidCustomAlias = "Requested custom alias contains illegal characters.";
+    public const string IncorrectPassword = "Submitted password is incorrect.";
+    public const string LinkNotFound = "Requested link does not exist.";
+    public const string InvalidDateFormat = "Dates must be in format of yyyy-MM-dd.";
 }
